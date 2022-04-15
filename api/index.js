8e962e7cd0ff71183fbcb15793a26ad16ff6602f
Martin <fedefiguerola@gmail.com>
2022-04-14 22:10:30 -0300

    commit done

--- api/index.js
+++ api/index.js
@@ -23,6 +23,6 @@ const { conn } = require('./src/db.js');
 // Syncing all the models at once.
 conn.sync({ force: false}).then(() => {
   server.listen(process.env.PORT, () => {
-    console.log('listening'); // eslint-disable-line no-console
+    console.log(`Puerto: ${process.env.PORT || 3001}` ); // eslint-disable-line no-console
   });
 });
--- api/src/app.js
+++ api/src/app.js
@@ -3,8 +3,6 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
-const  dotenv = require('dotenv')
-dotenv.config()
 
 require('./db.js');
 
@@ -20,7 +18,7 @@ server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
   //res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Origin', process.env.URL_FRONT || "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', "*"); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
