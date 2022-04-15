34db111ddddb9647d36fb277d6fbf240f7ca7c6c
Martin <fedefiguerola@gmail.com>
2022-04-15 11:37:29 -0300

    fixed

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const { conn } = require('./src/db.js');
 const PORT = process.env.PORT || 3001
 
 // Syncing all the models at once.
-conn.sync({ force: true}).then(() => {
+conn.sync({ force: false}).then(() => {
   server.listen(PORT, () => {
     console.log(`Servidor escuchando en el pueto ${PORT}` ); // eslint-disable-line no-console
   });
