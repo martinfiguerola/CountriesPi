70e912fa25e603a9e528c97546df99d14aa98663
Martin <fedefiguerola@gmail.com>
2022-04-14 22:21:01 -0300

    done

--- api/src/db.js
+++ api/src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
-let sequelize =
+/* let sequelize =
   process.env.NODE_ENV === "production"
     ? new Sequelize({
         database: DB_NAME,
@@ -30,12 +30,12 @@ let sequelize =
     : new Sequelize(
         `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`,
         { logging: false, native: false }
-      );
+      ); */
 
-/* const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-}); */
+});
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
